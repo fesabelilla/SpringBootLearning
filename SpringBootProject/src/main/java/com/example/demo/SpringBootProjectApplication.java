c53e8da5680bd9709e70dd4472d3a3659c66da14
fesabelilla <fesabelilla@gmail.com>
2021-06-19 21:27:31 +0600

    XML based constructor injection

--- SpringBootProject/src/main/java/com/example/demo/SpringBootProjectApplication.java
+++ SpringBootProject/src/main/java/com/example/demo/SpringBootProjectApplication.java
@@ -6,17 +6,25 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
 import com.example.demo.model.Coder;
+import com.example.demo.model.Software;
 
 @SpringBootApplication
-@ImportResource({"classpath:BeanConfig.xml"})
+//@ImportResource({"classpath:BeanConfig.xml"})
+@ImportResource({"classpath:SoftBean.xml"})
 public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
 	ApplicationContext context = SpringApplication.run(SpringBootProjectApplication.class, args);
 	
-		Coder c1 = (Coder) context.getBean("coder1"); 
-		System.out.println(c1.getName());
-		System.out.println(c1.getComputer().getBrand());
+		//Coder c1 = (Coder) context.getBean("coder1"); 
+		
+		//System.out.println(c1.getName());
+		//System.out.println(c1.getComputer().getBrand());
+	
+		Software software = (Software) context.getBean("software");
+		System.out.println(software.getName());
+		System.out.println(software.getComputer().getBrand());
+		
 	}
 
 }
---
+++ SpringBootProject/src/main/java/com/example/demo/model/Software.java
@@ -0,0 +1,73 @@
+package com.example.demo.model;
+
+import javax.annotation.PreDestroy;
+
+public class Software {
+	
+	// ctrl + space
+		private int id;
+		private String name;
+		private String language;
+		
+		private Computer computer;
+
+		// Alt+Shift+S
+		// https://stackoverflow.com/questions/53129538/content-autocomplete-in-spring-tool-suite-4/53143625
+
+		public Software() {
+			super();
+			System.out.println("No parameter. {Software class}");
+		}
+
+		public Software(int id, String name, String language, Computer computer) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.language = language;
+			this.computer = computer;
+			System.out.println("With Parameter. {Software class}");
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getLanguage() {
+			return language;
+		}
+
+		public void setLanguage(String language) {
+			this.language = language;
+		}
+
+		public Computer getComputer() {
+			return computer;
+		}
+
+		public void setComputer(Computer computer) {
+			this.computer = computer;
+		}
+		
+		//bofore destroy it will bw called
+		@PreDestroy
+		public void destroy() {
+			System.out.println("Coder class destroy");
+		}
+		
+		
+		//formatting ctrl + shift + f
+	
+
+}

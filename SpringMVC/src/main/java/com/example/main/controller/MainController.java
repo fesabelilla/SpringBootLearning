aa64536351169c995fb9ae719e795a96cb446244
fesabelilla <fesabelilla@gmail.com>
2021-07-01 00:39:19 +0600

    nan

--- SpringMVC/src/main/java/com/example/main/controller/MainController.java
+++ SpringMVC/src/main/java/com/example/main/controller/MainController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
 
 	@RequestMapping("/home")
-	public String homString() {
+	public String homeString() {
 		return "Home.html";
 	}
 	

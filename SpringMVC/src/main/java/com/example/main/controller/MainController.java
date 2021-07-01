3376313c4b4f18ffe760db56bece4b037f2d0c83
fesabelilla <fesabelilla@gmail.com>
2021-07-01 20:23:58 +0600

    Method Level @ModelAttribute

--- SpringMVC/src/main/java/com/example/main/controller/MainController.java
+++ SpringMVC/src/main/java/com/example/main/controller/MainController.java
@@ -3,6 +3,7 @@ package com.example.main.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +12,15 @@ import org.springframework.web.servlet.ModelAndView;
 import com.example.main.Model.Programmer;
 
 @Controller 
+@ControllerAdvice
 public class MainController {
 
+	@ModelAttribute
+	public void welcome(Model m) {
+		m.addAttribute("msg","Welcome to Spring Boot Project");
+	}
+	
+	
 	@RequestMapping("/home")
 	public String homeString() {
 		return "Home.html";
